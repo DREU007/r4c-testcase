67d7e79ccd4fca837a94aef40a79d7ae871a5fbe
DREU007 <dreu007@gmail.com>
2023-10-03 01:02:45 +0400

    Update signals.py tests.py

--- robots/signals.py
+++ robots/signals.py
@@ -1,36 +1,30 @@
 from django.db.models.signals import Signal, post_save
-from django.dispatch import reciever
+from django.dispatch import receiver
 from django.core.mail import send_mail
 from orders.models import Order
 from .models import Robot
 
 
 robot_created = Signal()
-order_saved = Signal()
 
 
-@reciever(post_save, sender=Robot)
+@receiver(post_save, sender=Robot)
 def check_robot_availability(sender, instance, created, **kwargs):
     if created:
         try:
             orders = Order.objects.filter(robot_serial=instance.serial)
-            if orders.exist():
-                order_saved.send(sender=Order, orders=orders)
-            except Order.DoesNotExist:
-                pass
+            data = Robot.objects.get(serial=instance.serial)
+            if orders.exists():
+                subject = "R4C: Робот в наличии" 
+                message = (
+                    "Добрый день!\n"
+                    f"Недавно вы интересовались нашим роботом модели {data.model}, версии {data.version} .\n"
+                    "Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами"
+                )
+                from_email = "info@r4c.com" 
+                to_emails = orders.values_list('customer__email', flat=True)
 
+                send_mail(subject, message, from_email, to_emails)
+        except Order.DoesNotExist:
+            pass
 
-@reciever(order_saved, sender=Order)
-def send_robot_available_email(sender, orders, **kwargs):
-    for order in orders:
-        try:
-            subject = "R4C: Робот в наличии" 
-            message = (
-                "Добрый день!\n"
-                f"Недавно вы интересовались нашим роботом модели {model}, версии {version} .\n"
-                "Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами"
-            )
-            from_email = "info@r4c.com" 
-            to_emails = [order.customer.email]
-
-            send_mail(subject, message, from_email, to_emails)
--- robots/tests.py
+++ robots/tests.py
@@ -1,9 +1,15 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, override_settings
+from django.core import mail
 from django.shortcuts import reverse
+
+from orders.models import Order
+from customers.models import Customer
+from .models import Robot
+from .signals import robot_created
 import json
 
 
-class RobotModelsTest(TestCase):
+class RobotsTest(TestCase):
     def setUp(self):
         self.client: Client = Client()
 
@@ -11,7 +17,7 @@ class RobotModelsTest(TestCase):
         data = {
             "model": "R2",
             "version": "D2",
-            "created": "2022-12-31 23:59:59"
+            "created": "2023-10-02 23:59:59"
         }
         json_data = json.dumps(data)
 
@@ -22,3 +28,38 @@ class RobotModelsTest(TestCase):
         self.assertJSONEqual(
             response.content, {'message': 'Data received successfully'}
         )
+
+    @override_settings(EMAIL_BACKEND='django.core.mail.backend.locmem.EmailBackend')
+    def test_email_on_availability(self):
+        robot_data = {
+            "serial": "R2-D2",
+            "model": "R2",
+            "version": "D2",
+            "created": "2023-10-02 23:59:59"
+        }
+
+        customer_data = {
+            "email": "customer@test.com"
+        }
+
+        order_data = {
+            "robot_serial": "R2-D2",
+        }
+
+
+        robot = Robot.objects.create(**robot_data)
+        customer = Customer.objects.create(**customer_data)
+        order = Order.objects.create(customer=customer, **order_data)
+        
+        robot_created.send(sender=Robot, instance=robot, created=True)
+
+        self.assertEqual(len(mail.outbox), 1)
+
+        email = mail.outbox[0]
+        message = (
+            "Добрый день!\n"
+            f"Недавно вы интересовались нашим роботом модели {robot_data['model']}, версии {rpbot_data['version']} .\n"
+            "Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами"
+        )
+        self.assertEqual(email.subject, "R4C: Робот в наличии")
+        self.assertIn(message)
