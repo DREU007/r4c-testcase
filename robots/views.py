5b3fd17f241c0214fcd2cd6d6ad4e70554a65716
DREU007 <dreu007@gmail.com>
2023-10-02 16:50:27 +0400

    Update views.py

--- robots/views.py
+++ robots/views.py
@@ -3,9 +3,12 @@ from django.views import View
 from django.http import JsonResponse, FileResponse
 from django.utils import timezone
 from django.db.models import Count, ExpressionWrapper, F, CharField
+
 from datetime import timedelta
 import json
 from openpyxl import Workbook
+from openpyxl import Border
+
 from .forms import RobotForm
 from .models import Robot
 
@@ -36,21 +39,46 @@ class RobotsExcelView(View):
             num_counted=Count('id', distinct=True)
         ).order_by('model', 'version')
         
-        # TODO: Generate Excel file
+        grouped_robots = {}
+        for key, group in itertools.groupby(robots, lambda k: k["model"]):
+            grouped_robots[key] = list(group)
+            
+        # Generate Excel file
         report_wb = Workbook()
+        report_wb.remove(report_wb.active)
         report_date = report_time_end.strftime('%d.%m.%Y')
 
-        report_ws = report_wb.create_sheet(f"Wk robots @{report_date}")
         report_file_title = f"wk-rep-robots-{report_date}"
 
-        col_titles = {
-            "model": "Модель",
-            "version": "Версия",
-            "num_counted": "Количество за неделю"
-        }
+        # Fill column titles
+        col_titles = [  
+                {"key": "model", "name": "Модель", "col_num": 1},
+                {"key": "version", "name": "Версия", "col_num": 2},
+                {"key": "num_counted", "name": "Количество за неделю", "col_num": 3},
+        ]
 
-        for row in report_ws.iter_rows(min_row=1, max_row=1, max_col=len(col_titles)):
-            for cell, title in zip(row, col_titles):
-                cell.value = title
-
+        for model, group in grouped_robots.items():
+            report_ws = report_wb.create_sheet(model)
+            set_ws_column_titles(report_ws, col_titles)
+            set_ws_data(report_ws, group, col_titles)
         # TODO: Return direct download link to excel
+
+        def set_ws_column_titles(ws, col_titles):
+            for title in col_titles:
+                ws.cell(
+                    row=1,
+                    column=title["col_num"],
+                    value=title["name"],
+                )
+        def set_ws_data(ws, data, col_titles):
+            current_row = 2
+            for item in data:
+                for title in col_titles:
+                    ws.cell(
+                        row=current_row,
+                        column=title["col_num"],
+                        value=item[title["key"]],
+                    )
+                current_row += 1
+            
+            
