403449e14441aa64a55044cdfa3238d4771f57bf
DREU007 <dreu007@gmail.com>
2023-10-02 21:34:22 +0400

    Fix views.py

--- robots/views.py
+++ robots/views.py
@@ -10,6 +10,10 @@ class PostJson(View):
         data = json.loads(request.body)
         form = RobotForm(data)
         if form.is_valid():
+            serial = (
+                f'{form.cleaned_data["model"]}-{form.cleaned_data["version"]}'
+            )
+            form.instance.serial = serial
             form.save()
             return JsonResponse({'message': 'Data received successfully'})
         errors = form.errors.as_join()
