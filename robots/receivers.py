2142b133f849e672b23f25568ec590f483ee81cb
DREU007 <dreu007@gmail.com>
2023-10-03 23:07:54 +0400

    Fix receivers.py; Update apps.py

--- robots/receivers.py
+++ robots/receivers.py
@@ -1,6 +1,8 @@
-from django.db.models.signals import Signal, post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
+from R4C import settings
+
 from orders.models import Order
 from .models import Robot
 
@@ -10,18 +12,16 @@ def send_email_on_availabilty(sender, instance, created, **kwargs):
     if created:
         try:
             orders = Order.objects.filter(robot_serial=instance.serial)
-            data = Robot.objects.get(serial=instance.serial)
             if orders.exists():
-                subject = "R4C: Робот в наличии" 
-                message = (
-                    "Добрый день!\n"
-                    f"Недавно вы интересовались нашим роботом модели {data.model}, версии {data.version} .\n"
-                    "Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами"
-                )
-                from_email = "info@r4c.com" 
-                to_emails = list(orders.values_list('customer__email', flat=True))
+                subject = "R4C: Робот в наличии"
+
+                message = f"""Добрый день!
+Недавно вы интересовались нашим роботом модели {instance.model}, версии {instance.version}.
+Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами."""  # noqa
+
+                from_email = settings.EMAIL_HOST_USER
+                to_emails = orders.values_list('customer__email', flat=True)
 
                 send_mail(subject, message, from_email, to_emails)
         except Order.DoesNotExist:
             pass
-
